ad5be15ea9715b509e7470510318580b2ec4b8c0
MarttiWu <69631800+MarttiWu@users.noreply.github.com>
2020-10-02 12:31:25 +0800

    Update Boids.py

--- Boids.py
+++ Boids.py
@@ -3,16 +3,13 @@ import random
 import math
 import numpy as np
 
-radius=40
+a_radius=50
+s_radius=20
+c_radius=40
 popsize=100
 maxspeed=5.0
 
-def norm(v):
-    sum = np.zeros(1).astype('float128')
-    for i in range(len(v)):
-        sum += v[i]**2
-    return sum**0.5
-        
+numbers = list(range(-15,-1)) + list(range(1,15))
 
 pg.init()
 screen = pg.display.set_mode((800,600))
@@ -39,15 +36,16 @@ class Bird(pg.sprite.Sprite):
         super().__init__()
         self.image = img['Nbird']
         self.rect = self.image.get_rect()
-        # 定位
         self.rect.center = (x, y)
         self.x = x
         self.y = y
         self.dx = dx
         self.dy = dy
-        print('dx:',self.dx)
-        print('dy:',self.dy)
-        self.neighbors = []
+        #print('dx:',self.dx)
+        #print('dy:',self.dy)
+        self.aneighbors = []
+        self.cneighbors = []
+        self.sneighbors = []
         
     def draw(self):
         
@@ -68,6 +66,13 @@ class Bird(pg.sprite.Sprite):
             self.dy = -self.dy
             self.y += self.dy
         '''
+        if self.is_collided_with(obstacles):
+            self.dx = -self.dx
+            self.dy = -self.dy
+            #self.x += self.dx/np.linalg.norm((self.dx,self.dy))*70
+            #self.y += self.dy/np.linalg.norm((self.dx,self.dy))*70
+            self.x += random.choice(numbers)*2
+            self.x += random.choice(numbers)*2
             
         if self.dx>0 and self.dy>0:
             self.image = img['SEbird']
@@ -91,21 +96,27 @@ class Bird(pg.sprite.Sprite):
         self.y += self.dy
         
     def find_neighbors(self,other):
-        self.neighbors.clear()
+        self.cneighbors.clear()
+        self.sneighbors.clear()
+        self.aneighbors.clear()
         for o in other:
             if o!=self:
-                if math.dist([self.x,self.y],[o.x,o.y]) < radius:
-                    self.neighbors.append(o)
+                if math.dist([self.x,self.y],[o.x,o.y]) < c_radius:
+                    self.cneighbors.append(o)
+                if math.dist([self.x,self.y],[o.x,o.y]) < s_radius:
+                    self.sneighbors.append(o)
+                if math.dist([self.x,self.y],[o.x,o.y]) < a_radius:
+                    self.aneighbors.append(o)
                     
     def cohension(self):
         cV = np.zeros(2).astype('float128')
-        print('type', type(cV[0]))
-        if not self.neighbors:
+        #print('type', type(cV[0]))
+        if not self.cneighbors:
             return cV
-        for n in self.neighbors:
+        for n in self.cneighbors:
             cV[0]+=n.x
             cV[1]+=n.y
-        cV/=len(self.neighbors)
+        cV/=len(self.cneighbors)
         cV[0]-=self.x
         cV[1]-=self.y
         
@@ -122,12 +133,12 @@ class Bird(pg.sprite.Sprite):
         
     def separation(self):
         sV = np.zeros(2).astype('float128')
-        if not self.neighbors:
+        if not self.sneighbors:
             return sV
-        for n in self.neighbors:
+        for n in self.sneighbors:
             sV[0]+=self.x-n.x
             sV[1]+=self.y-n.y
-        sV/=len(self.neighbors)
+        sV/=len(self.sneighbors)
         
         uu = np.linalg.norm(sV)
         if uu==0:
@@ -141,12 +152,12 @@ class Bird(pg.sprite.Sprite):
         
     def alignment(self):
         aV = np.zeros(2).astype('float128')
-        if not self.neighbors:
+        if not self.aneighbors:
             return aV
-        for n in self.neighbors:
+        for n in self.aneighbors:
             aV[0]+=n.dx
             aV[1]+=n.dy
-        aV/=len(self.neighbors)
+        aV/=len(self.aneighbors)
         
         uu = np.linalg.norm(aV)
         if uu==0:
@@ -177,10 +188,17 @@ class Bird(pg.sprite.Sprite):
         if int(V[0]) or int(V[1]):
             self.dx = int(V[0])
             self.dy = int(V[1])
+            
+    def is_collided_with(self, obstacles):
+        col = False
+        for ob in obstacles:
+            if self.rect.colliderect(ob.rect):
+                col = True
+        return col
 
 
 
-numbers = list(range(-15,-1)) + list(range(1,15))
+
 
 def init_boids():
     birds = [Bird(random.randint(40,1360),random.choice(numbers),random.randint(40,860),random.choice(numbers)) for i in range(popsize)]
@@ -196,7 +214,27 @@ clock = pg.time.Clock()
 
 
 
+obimgsize = (30,30)
+obimg = pg.transform.scale(pg.image.load('images/block.png'), obimgsize)
 
+
+
+obstacles = []
+
+OBSTACLE = pg.sprite.Group()
+class Obstacle(pg.sprite.Sprite):
+    def __init__(self,x,y):
+        super().__init__()
+        self.image = obimg
+        self.rect = self.image.get_rect()
+        self.rect.center = (x, y)
+        self.x = x
+        self.y = y
+    def draw(self):
+        self.rect.center = (self.x,self.y)
+
+
+'''
 def drawob():
     mouses = pg.mouse.get_pos()
 
@@ -210,6 +248,9 @@ def drawob():
     
     rect.centerx = mouses[0]
     rect.centery = mouses[1]
+'''
+
+
 
 
 
@@ -228,17 +269,46 @@ smallfont = pg.font.SysFont('Arial',20)
 
 birds,boids = init_boids()
 running = True
+START=False
+OBon=False
 while running:
     clock.tick(40)
     for e in pg.event.get():
         if e.type == pg.QUIT:
             running = False
+            
+        #print('OBon',OBon)
+        if e.type == pg.MOUSEBUTTONDOWN and OBon:
+            #if 100 <= mouse[0] <= 700 and 100 <= mouse[1] <= 500:
+            #print('Hiiiiiiiiiii')
+            pos = pg.mouse.get_pos()
+            if 0 <= pos[1] <= 540:
+                ob = Obstacle(pos[0],pos[1])
+                obstacles.append(ob)
+                OBSTACLE.add(ob)
+            
         if e.type == pg.MOUSEBUTTONDOWN:
             if 600 <= mouse[0] <= 700 and 560 <= mouse[1] <= 580:
                 pg.quit()
             if 100 <= mouse[0] <= 200 and 560 <= mouse[1] <= 580:
+                START=True
+                
+            if 250 <= mouse[0] <= 350 and 560 <= mouse[1] <= 580:
                 birds,boids = init_boids()
-        
+                obstacles.clear()
+                OBSTACLE.empty()
+                START=True
+                OBon=False
+                
+            if 450 <= mouse[0] <= 550 and 560 <= mouse[1] <= 580:
+                OBon=True
+                #print('hi')
+            
+            '''
+            if 250 <= mouse[0] <= 350 and 560 <= mouse[1] <= 580:
+                OBon=True
+                print('Obon',OBon)
+            '''
     
     
     
@@ -246,18 +316,27 @@ while running:
     #buttons = pg.mouse.get_pressed()
     #if buttons[0]:
     #    drawob()
-        
             
             
     screen.blit(background,(0,0))
-    for bird in birds:
-        #print('hi')
-        bird.draw()
+    
+    if START:
+    
+        for ob in obstacles:
+            ob.draw()
+            #print('x:',ob.x)
+            #print('y:',ob.y)
+        
+        OBSTACLE.draw(screen)
+        
+        
+        for bird in birds:
+            bird.draw()
 
-    for bird in birds:
-        bird.update_direction(birds)
+        for bird in birds:
+            bird.update_direction(birds)
 
-    boids.draw(screen)
+        boids.draw(screen)
     
     
     
@@ -273,7 +352,19 @@ while running:
         pg.draw.rect(screen,color_light,[100,560,100,20])
     else:
         pg.draw.rect(screen,color_dark,[100,560,100,20])
-    screen.blit(smallfont.render('Restart' , True , color) , (100+30,560+3))
+    screen.blit(smallfont.render('Start' , True , color) , (100+30,560+3))
+    
+    if 250 <= mouse[0] <= 350 and 560 <= mouse[1] <= 580:
+        pg.draw.rect(screen,color_light,[250,560,100,20])
+    else:
+        pg.draw.rect(screen,color_dark,[250,560,100,20])
+    screen.blit(smallfont.render('Restart' , True , color) , (250+30,560+3))
+    
+    if 450 <= mouse[0] <= 550 and 560 <= mouse[1] <= 580:
+        pg.draw.rect(screen,color_light,[450,560,100,20])
+    else:
+        pg.draw.rect(screen,color_dark,[450,560,100,20])
+    screen.blit(smallfont.render('Obstacle' , True , color) , (450+20,560+3))
     #####
     
     
