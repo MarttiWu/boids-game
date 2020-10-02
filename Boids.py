dad5d20c0c9b536c1461a2cbdbf62d51907f796a
MarttiWu <69631800+MarttiWu@users.noreply.github.com>
2020-10-02 15:00:58 +0800

    Update Boids.py

--- Boids.py
+++ Boids.py
@@ -3,14 +3,28 @@ import random
 import math
 import numpy as np
 
+'''
+    boids behavior parameters
+'''
+#####
 a_radius=50
 s_radius=20
 c_radius=40
 popsize=100
 maxspeed=5.0
-
 numbers = list(range(-15,-1)) + list(range(1,15))
+#####
 
+'''
+    image size
+'''
+#####
+bdimgsize=(20,20) #bird image size
+obimgsize = (30,30) #obstacle image size
+#####
+
+
+#initialize window
 pg.init()
 screen = pg.display.set_mode((800,600))
 pg.display.set_caption("Boids Game")
@@ -18,8 +32,8 @@ background = pg.image.load('images/sky.jpg')
 background.convert()
 screen.blit(background,(0,0))
 
-bdimgsize=(20,20)
 
+#load images
 img = {'Ebird':pg.transform.scale(pg.image.load('images/Ebird.png'), bdimgsize),
         'Wbird':pg.transform.scale(pg.image.load('images/Wbird.png'), bdimgsize),
         'Sbird':pg.transform.scale(pg.image.load('images/Sbird.png'), bdimgsize),
@@ -29,6 +43,8 @@ img = {'Ebird':pg.transform.scale(pg.image.load('images/Ebird.png'), bdimgsize),
         'SEbird':pg.transform.scale(pg.image.load('images/SEbird.png'), bdimgsize),
         'SWbird':pg.transform.scale(pg.image.load('images/SWbird.png'), bdimgsize)
 }
+obimg = pg.transform.scale(pg.image.load('images/block.png'), obimgsize)
+
 
 class Bird(pg.sprite.Sprite):
     
@@ -41,14 +57,12 @@ class Bird(pg.sprite.Sprite):
         self.y = y
         self.dx = dx
         self.dy = dy
-        #print('dx:',self.dx)
-        #print('dy:',self.dy)
         self.aneighbors = []
         self.cneighbors = []
         self.sneighbors = []
         
     def draw(self):
-        
+        #goes to the other side when reaches the boundaries
         if self.x>800:
             self.x = 0
         if self.x<0:
@@ -57,23 +71,13 @@ class Bird(pg.sprite.Sprite):
             self.y = 0
         if self.y<0:
             self.y = 600
-        '''
-        if self.x>780 or self.x<0:
-            self.dx = -self.dx
-            self.x += self.dx
-
-        if self.y>580 or self.y<0:
-            self.dy = -self.dy
-            self.y += self.dy
-        '''
+        #check for collision with obstacles
         if self.is_collided_with(obstacles):
             self.dx = -self.dx
             self.dy = -self.dy
-            #self.x += self.dx/np.linalg.norm((self.dx,self.dy))*70
-            #self.y += self.dy/np.linalg.norm((self.dx,self.dy))*70
             self.x += random.choice(numbers)*2
             self.x += random.choice(numbers)*2
-            
+        #change image for different directions
         if self.dx>0 and self.dy>0:
             self.image = img['SEbird']
         elif self.dx>0 and self.dy<0:
@@ -109,8 +113,7 @@ class Bird(pg.sprite.Sprite):
                     self.aneighbors.append(o)
                     
     def cohension(self):
-        cV = np.zeros(2).astype('float128')
-        #print('type', type(cV[0]))
+        cV = np.zeros(2)
         if not self.cneighbors:
             return cV
         for n in self.cneighbors:
@@ -119,20 +122,16 @@ class Bird(pg.sprite.Sprite):
         cV/=len(self.cneighbors)
         cV[0]-=self.x
         cV[1]-=self.y
-        
+        #prevent divided by 0
         uu = np.linalg.norm(cV)
         if uu==0:
             uu=0.01
-                
         cV = (cV/uu)*maxspeed
-        #cV = (cV/np.linalg.norm(cV))*maxspeed
         
-        #cV = (cV/norm(cV))*maxspeed
-        #print(cV)
         return cV
         
     def separation(self):
-        sV = np.zeros(2).astype('float128')
+        sV = np.zeros(2)
         if not self.sneighbors:
             return sV
         for n in self.sneighbors:
@@ -141,49 +140,43 @@ class Bird(pg.sprite.Sprite):
         sV/=len(self.sneighbors)
         
         uu = np.linalg.norm(sV)
+        #prevent divided by 0
         if uu==0:
-            uu=0.1
+            uu=0.01
                 
         sV = (sV/uu)*maxspeed
-        
-        #sV = (sV/np.linalg.norm(sV))*maxspeed
-        #sV = (sV/norm(sV))*maxspeed
+    
         return sV
         
     def alignment(self):
-        aV = np.zeros(2).astype('float128')
+        aV = np.zeros(2)
         if not self.aneighbors:
             return aV
         for n in self.aneighbors:
             aV[0]+=n.dx
             aV[1]+=n.dy
         aV/=len(self.aneighbors)
-        
+        #prevent divided by 0
         uu = np.linalg.norm(aV)
         if uu==0:
-            uu=0.1
+            uu=0.01
                 
         aV = (aV/uu)*maxspeed
-        
-        #aV = (aV/np.linalg.norm(aV))*maxspeed
-        #aV = (aV/norm(aV))*maxspeed
+
         return aV
         
     def update_direction(self,other):
         self.find_neighbors(other)
-        V = np.zeros(2).astype('float128')
+        V = np.zeros(2)
         V += self.cohension()
         V += self.separation()
         V += self.alignment()
-        
+        #prevent divided by 0
         uu = np.linalg.norm(V)
         if uu==0:
-            uu=0.1
+            uu=0.01
                 
         V = (V/uu)*maxspeed
-        
-        #V = (V/np.linalg.norm(V))*maxspeed
-        #V = (V/norm(V))*maxspeed
         
         if int(V[0]) or int(V[1]):
             self.dx = int(V[0])
@@ -197,9 +190,6 @@ class Bird(pg.sprite.Sprite):
         return col
 
 
-
-
-
 def init_boids():
     birds = [Bird(random.randint(40,1360),random.choice(numbers),random.randint(40,860),random.choice(numbers)) for i in range(popsize)]
 
@@ -211,11 +201,6 @@ def init_boids():
     return birds,boids
 
 clock = pg.time.Clock()
-
-
-
-obimgsize = (30,30)
-obimg = pg.transform.scale(pg.image.load('images/block.png'), obimgsize)
 
 
 
@@ -235,35 +220,24 @@ class Obstacle(pg.sprite.Sprite):
 
 
 '''
-def drawob():
-    mouses = pg.mouse.get_pos()
-
-    size=(50,50)
-    ob = pg.Surface(size)
-    ob.fill((255,255,255))
-    pg.draw.rect(ob, (255,0,0), ob.get_rect())
-    rect = ob.get_rect()
-    ob.fill((255,255,255))
-    #rect.center = (mouses[0],mouses[1])
-    
-    rect.centerx = mouses[0]
-    rect.centery = mouses[1]
+    buttons' parameters...
 '''
-
-
-
-
-
-
 #############
 color = (255,255,255)
 
-color_light = (170,170,170)
-  
-color_dark = (100,100,100)
-  
-smallfont = pg.font.SysFont('Arial',20)
+st_light = (170,170,170)
+st_dark = (100,100,100)
 
+re_light = (255,102,102)
+re_dark = (255,0,0)
+
+ob_light = (51,255,255)
+ob_dark = (0,204,204)
+
+qu_light = (32,32,32)
+qu_dark = (0,0,0)
+
+buttonfont = pg.font.SysFont('Arial',20)
 ###############
 
 
@@ -277,10 +251,7 @@ while running:
         if e.type == pg.QUIT:
             running = False
             
-        #print('OBon',OBon)
         if e.type == pg.MOUSEBUTTONDOWN and OBon:
-            #if 100 <= mouse[0] <= 700 and 100 <= mouse[1] <= 500:
-            #print('Hiiiiiiiiiii')
             pos = pg.mouse.get_pos()
             if 0 <= pos[1] <= 540:
                 ob = Obstacle(pos[0],pos[1])
@@ -290,6 +261,7 @@ while running:
         if e.type == pg.MOUSEBUTTONDOWN:
             if 600 <= mouse[0] <= 700 and 560 <= mouse[1] <= 580:
                 pg.quit()
+                
             if 100 <= mouse[0] <= 200 and 560 <= mouse[1] <= 580:
                 START=True
                 
@@ -302,70 +274,55 @@ while running:
                 
             if 450 <= mouse[0] <= 550 and 560 <= mouse[1] <= 580:
                 OBon=True
-                #print('hi')
-            
-            '''
-            if 250 <= mouse[0] <= 350 and 560 <= mouse[1] <= 580:
-                OBon=True
-                print('Obon',OBon)
-            '''
-    
-    
-    
-    
-    #buttons = pg.mouse.get_pressed()
-    #if buttons[0]:
-    #    drawob()
-            
             
     screen.blit(background,(0,0))
-    
+    '''
+        Game start
+    '''
     if START:
     
         for ob in obstacles:
             ob.draw()
-            #print('x:',ob.x)
-            #print('y:',ob.y)
         
         OBSTACLE.draw(screen)
         
-        
         for bird in birds:
             bird.draw()
-
+            
         for bird in birds:
             bird.update_direction(birds)
-
+            
         boids.draw(screen)
     
     
-    
     mouse = pg.mouse.get_pos()
-    ####
+    '''
+        Functional buttons
+    '''
+    #Quit button
     if 600 <= mouse[0] <= 700 and 560 <= mouse[1] <= 580:
-        pg.draw.rect(screen,color_light,[600,560,100,20])
+        pg.draw.rect(screen,qu_light,[600,560,100,20])
     else:
-        pg.draw.rect(screen,color_dark,[600,560,100,20])
-    screen.blit(smallfont.render('Quit' , True , color) , (600+35,560+3))
-    
+        pg.draw.rect(screen,qu_dark,[600,560,100,20])
+    screen.blit(buttonfont.render('Quit' , True , color) , (600+35,560+3))
+    #Start button
     if 100 <= mouse[0] <= 200 and 560 <= mouse[1] <= 580:
-        pg.draw.rect(screen,color_light,[100,560,100,20])
+        pg.draw.rect(screen,st_light,[100,560,100,20])
     else:
-        pg.draw.rect(screen,color_dark,[100,560,100,20])
-    screen.blit(smallfont.render('Start' , True , color) , (100+30,560+3))
-    
+        pg.draw.rect(screen,st_dark,[100,560,100,20])
+    screen.blit(buttonfont.render('Start' , True , color) , (100+30,560+3))
+    #Restart button
     if 250 <= mouse[0] <= 350 and 560 <= mouse[1] <= 580:
-        pg.draw.rect(screen,color_light,[250,560,100,20])
+        pg.draw.rect(screen,re_light,[250,560,100,20])
     else:
-        pg.draw.rect(screen,color_dark,[250,560,100,20])
-    screen.blit(smallfont.render('Restart' , True , color) , (250+30,560+3))
-    
+        pg.draw.rect(screen,re_dark,[250,560,100,20])
+    screen.blit(buttonfont.render('Restart' , True , color) , (250+30,560+3))
+    #Obstacle button
     if 450 <= mouse[0] <= 550 and 560 <= mouse[1] <= 580:
-        pg.draw.rect(screen,color_light,[450,560,100,20])
+        pg.draw.rect(screen,ob_light,[450,560,100,20])
     else:
-        pg.draw.rect(screen,color_dark,[450,560,100,20])
-    screen.blit(smallfont.render('Obstacle' , True , color) , (450+20,560+3))
-    #####
+        pg.draw.rect(screen,ob_dark,[450,560,100,20])
+    screen.blit(buttonfont.render('Obstacle' , True , color) , (450+20,560+3))
     
     
     pg.display.update()
